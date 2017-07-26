2e32e57dc3549d23e1eef4245235cf6240b42389
blooddot <blooddot@163.com>
2017-07-26 23:57:07 +0800

    上传啊啊啊

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// JavaScript Document
+$(document).ready(function(){
+	$("#cont li:nth-child(4n-1)").attr("class"," box boxsec");
+	$("#cont li:nth-child(4n)").attr("class"," box boxsec");
+    $("#cont li:nth-child(4n-2)").attr("class"," box boxcolorone");
+    $("#cont li:nth-child(4n)").attr("class"," box boxsec boxcolorsec");
+    
+})
