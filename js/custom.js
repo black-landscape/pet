0020e426e08192cbe879824a198158ee720d198b
blooddot <blooddot@163.com>
2017-08-08 01:28:37 +0800

    pet

---
+++ js/custom.js
@@ -0,0 +1,237 @@
+var pet_bed_json = [{
+		"itemNo": "KWDB071",
+		"name": "DOG BED",
+		"size": "SMALL/45X30X12CM,MEDIUM/55X40X15CM,LARGE/65X50X17CM",
+		"oem": "AVAILABLE"
+	},
+	{
+		"itemNo": "KWDB072",
+		"name": "DOG BED",
+		"size": "80CMX65CMX10CM",
+		"oem": "AVAILABLE"
+	},
+	{
+		"itemNo": "KWDB073",
+		"name": "DOG BED",
+		"size": "65X50X17CM",
+		"oem": "AVAILABLE"
+	},
+	{
+		"itemNo": "KWDB074",
+		"name": "DOG BED",
+		"size": "SMALL/45X30X12CM,MEDIUM/55X40X15CM,LARGE/65X50X17CM",
+		"oem": "AVAILABLE"
+	},
+	{
+		"itemNo": "KWDB075",
+		"name": "DOG BED",
+		"size": "SMALL/45X30X12CM,MEDIUM/55X40X15CM,LARGE/65X50X17CM",
+		"oem": "AVAILABLE"
+	},
+	{
+		"itemNo": "KWDB076",
+		"name": "DOG BED",
+		"size": "SMALL/45X30X12CM,MEDIUM/55X40X15CM,LARGE/65X50X17CM",
+		"oem": "AVAILABLE"
+	},
+	{
+		"itemNo": "KWDB077",
+		"name": "DOG BED",
+		"size": "SMALL/45X30X12CM,MEDIUM/55X40X15CM,LARGE/65X50X17CM",
+		"oem": "AVAILABLE"
+	},
+	{
+		"itemNo": "KWDB078",
+		"name": "DOG BED",
+		"size": "55X40X22CM",
+		"oem": "AVAILABLE"
+	},
+	{
+		"itemNo": "KWDB079",
+		"name": "SQUARE DOG BED",
+		"size": "XS: 44X44X17,M: 52X52X17CML: 60X60X17CM",
+		"oem": "AVAILABLE"
+	},
+	{
+		"itemNo": "KWDB080",
+		"name": "DOG BED",
+		"size": "50X47CM",
+		"oem": "AVAILABLE"
+	},
+	{
+		"itemNo": "KWDB081",
+		"name": "PET BED FOR SMALL ANIMALS",
+		"size": "55CMX45CM",
+		"oem": "AVAILABLE"
+	},
+	{
+		"itemNo": "KWDB082",
+		"name": "PET BED FOR SMALL ANIMALS",
+		"size": "50X47CM",
+		"oem": "AVAILABLE"
+	},
+	{
+		"itemNo": "KWDB083",
+		"name": "Cat Bed",
+		"size": "44X43CM",
+		"oem": "AVAILABLE"
+	},
+	{
+		"itemNo": "KWDB084",
+		"name": "Cat Bed",
+		"size": "40CM",
+		"oem": "AVAILABLE"
+	},
+	{
+		"itemNo": "KWDB085",
+		"name": "Cat Bed",
+		"size": "40CMX35CM",
+		"oem": "AVAILABLE"
+	}
+];
+
+var dog_clothing_json = [{
+	"itemNo": "KWPC0707",
+	"name": "DOG CLOTH",
+	"oem": "AVAILABLE"
+}, {
+	"itemNo": "KWPC0708",
+	"name": "DOG CLOTH",
+	"oem": "AVAILABLE"
+}, {
+	"itemNo": "KWPC0709",
+	"name": "DOG CLOTH",
+	"oem": "AVAILABLE"
+}, {
+	"itemNo": "KWPC0710",
+	"name": "DOG CLOTH",
+	"oem": "AVAILABLE"
+}, {
+	"itemNo": "KWPC0711",
+	"name": "DOG CLOTH",
+	"oem": "AVAILABLE"
+}, {
+	"itemNo": "KWPC0712",
+	"name": "DOG CLOTH",
+	"oem": "AVAILABLE"
+}, {
+	"itemNo": "KWPC0713",
+	"name": "FASHION DOG CLOTH",
+	"oem": "AVAILABLE"
+}, {
+	"itemNo": "KWPC0714",
+	"name": "FASHION DOG CLOTH",
+	"oem": "AVAILABLE"
+}];
+
+var dog_collar_leash = [{
+		"itemNo": "KWDCL801",
+		"name": "DOG COLLAR",
+		"oem": "AVAILABLE"
+	}, {
+		"itemNo": "KWDCL802",
+		"name": "DOG HARNESS",
+		"oem": "AVAILABLE"
+	}, {
+		"itemNo": "KWDCL803",
+		"name": "DOG HARNESS",
+		"oem": "AVAILABLE"
+	}, {
+		"itemNo": "KWDCL804",
+		"name": "PET COLOAR",
+		"oem": "AVAILABLE"
+	}, {
+		"itemNo": "KWDCL805",
+		"name": "DOG HARNESS&LEASH",
+		"oem": "AVAILABLE"
+	}, {
+		"itemNo": "KWDCL806",
+		"name": "LEATHER  DOG COLOAR",
+		"oem": "AVAILABLE"
+	}, {
+		"itemNo": "KWDCL807",
+		"name": "RIVET DOG COLOAR",
+		"oem": "AVAILABLE"
+	}, {
+		"itemNo": "KWDCL808",
+		"name": "RIVET DOG COLOAR",
+		"oem": "AVAILABLE"
+	}, {
+		"itemNo": "KWDCL809",
+		"name": "RIVET DOG HARNESS",
+		"oem": "AVAILABLE"
+	}, {
+		"itemNo": "KWDL9001",
+		"name": "DOG OUTDOOR RUNNING  LEASH SET PACKING",
+		"oem": "AVAILABLE"
+	}, {
+		"itemNo": "KWDL9002",
+		"name": "DOG LEASH",
+		"oem": "AVAILABLE"
+	}, {
+		"itemNo": "KWDL9003",
+		"name": "DOG LEASH WITH VARIOUS COLOR",
+		"oem": "AVAILABLE"
+	},
+	{
+		"itemNo": "KWDL9004",
+		"name": "DOG LEASH TWO SIDE USE",
+		"oem": "AVAILABLE"
+	},
+	{
+		"itemNo": "KWDL9005",
+		"name": "DOG LEASH TWO SIDE USE",
+		"oem": "AVAILABLE"
+	},
+	{
+		"itemNo": "KWDL9006",
+		"name": "PET CAR SAFE LEASH",
+		"oem": "AVAILABLE"
+	},
+	{
+		"itemNo": "KWDL9007",
+		"name": "PET CAR SAFE LEASH",
+		"oem": "AVAILABLE"
+	}
+];
+
+var pet_toys = [{
+		"itemNo": "KWPT1001",
+		"name": "Dog playing Ball",
+		"oem": "AVAILABLE"
+	}, {
+		"itemNo": "KWPT1002",
+		"name": "Pet Rope Toy",
+		"oem": "AVAILABLE"
+	},
+	{
+		"itemNo": "KWPT1003",
+		"name": "Pet Chew Toy",
+		"oem": "AVAILABLE"
+	},
+	{
+		"itemNo": "KWPT1004",
+		"name": "Pet Tunnel",
+		"oem": "AVAILABLE"
+	},
+	{
+		"itemNo": "KWPT1005",
+		"name": "Pet Tunnel",
+		"oem": "AVAILABLE"
+	},
+	{
+		"itemNo": "KWPT1006",
+		"name": "Feather Wand Cat Toy",
+		"oem": "AVAILABLE"
+	},
+	{
+		"itemNo": "KWPT1007",
+		"name": "Cat Accessory",
+		"oem": "AVAILABLE"
+	},
+	{
+		"itemNo": "KWPT1008",
+		"name": "Cat Accessory",
+		"oem": "AVAILABLE"
+	}
+];
